2c0834dd7e45a03b31204612366490a77788ac54
cuntiao <13058262524@163.com>
2020-04-01 14:13:52 +0800

    shopmanager的模板

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+// 使用element-ui组件
+Vue.use(ElementUI)
 
 new Vue({
   router,
